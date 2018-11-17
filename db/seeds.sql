9e9a53d181110b5d332d46567e63692eba7403ea
Ian Richard <ianrichard03@gmail.com>
2018-11-17 08:58:55 -0600

    Setup packages, DB and GitIgnore.

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name,devoured)
+VALUES("cheeseburger",false);
+
+INSERT INTO burgers(burger_name,devoured)
+VALUES("hamburger",false);
+
+INSERT INTO burgers(burger_name,devoured)
+VALUES("veggieburger",false);
