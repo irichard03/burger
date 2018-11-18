25bed7914089a0048c164d4692545eb475182205
Ian Richard <ianrichard03@gmail.com>
2018-11-17 21:52:23 -0600

    Added selectAll() to orm.

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const mysql = require('mysql');
 
+
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    host: 'localhost',
+    port: 3306,
     user: 'root',
     password: 'password',
     database: 'burgers_db'
@@ -15,6 +16,7 @@ connection.connect(function(err) {
         return;
     }
     console.log("connected as id: " + connection.threadId);
+  
 });
 
 module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,24 @@
-//TO DO CREATE DB CALLS
+const connection = require('connection');
+
+
+
+const orm = {
+    //method for selecting all rows in the database, callback for results.
+    selectAll: function(column1,table,callback) {
+        let myQuery = "SELECT ?? FROM ??;";
+        connection.query(myQuery,[column1,table],function(err,result){
+            if(err){
+                throw(err);
+            }
+            callback(result);
+        });
+    },
+
+    //TODO insertOne
+
+    //TODO updateOne
+
+
+}
+
+module.exports = orm;
