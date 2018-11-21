1ce0368e53911df0ccba96bba4fd5602579c655e
Ian Richard <ianrichard03@gmail.com>
2018-11-20 22:00:01 -0600

    updated connections.js

--- models/burger.js
+++ models/burger.js
@@ -7,7 +7,6 @@ const burger = {
     //method takes in a callback function as argument
     //calls orm select all method passing column, table, and anon callback function.
     //then calls the callback that it received as an argument from..
-    //uh I guess the controller?
     selectAll : function(callback) {
         orm.selectAll("burger_name", "devoured", "burgers",function(response){
         callback(response);
