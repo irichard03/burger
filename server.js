b9503425e866f962fdf8a0b62082065fdde34077
Ian Richard <ianrichard03@gmail.com>
2018-11-27 23:52:26 -0600

    renamed port.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 
 //server listens for client requets.
-const PORT = process.env.SERVERPORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
